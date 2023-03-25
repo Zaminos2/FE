481cd07c37c54b5f38ae4a7488b6cb42f85c62b2
Zaminos2 <124680112+Zaminos2@users.noreply.github.com>
2023-03-25 16:07:20 +0100

    Delete JS directory

--- JS/JS 9/JS HW 07.03.2023/script.js
+++
@@ -1,5 +0,0 @@
-console.log(
-    [1, 4, 3, 0, 4, 5, 4 ]
-    .filter(Element => !(Element % 2))
-    .reduceRight((accmulator, Element) => accmulator + Math.sqrt(Element), 0)
-)
--- JS/JS HW/Home work 28.03.2023/2/scr.js
+++
@@ -1,7 +0,0 @@
-const num1 = 43 * 5;
-const num2 = 25 / 5;
-if (num1 > num2) {console.log(num2)
-    
-} else{console.log(num1)
-
-}
